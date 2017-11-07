5a017c84017a7b40132aab02043777be13d67ac8
zawwaizoe <rohinjar456+1@gmail.com>
2017-11-07 05:58:45 -0800

    Activity_Post

--- app/src/main/java/com/example/kryptonite/myblog/Main.java
+++ app/src/main/java/com/example/kryptonite/myblog/Main.java
@@ -1,7 +1,10 @@
 package com.example.kryptonite.myblog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.action_add){
+            startActivity(new Intent(Main.this,Post_Activity.class));
+        }
+
+
+        return  super.onOptionsItemSelected(item);
     }
 }
