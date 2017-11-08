4c09c61c3652190579a05e5ea711a24ab94743d0
zawwaizoe <rohinjar456+1@gmail.com>
2017-11-08 09:13:59 -0800

    8-11-2017  last update

--- app/src/main/java/com/example/kryptonite/myblog/Blog.java
+++ app/src/main/java/com/example/kryptonite/myblog/Blog.java
@@ -8,5 +8,35 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class Blog{
+private String title,desc,image;
 
+    public Blog(String title, String desc, String image) {
+        this.title = title;
+        this.desc = desc;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
--- app/src/main/java/com/example/kryptonite/myblog/Main.java
+++ app/src/main/java/com/example/kryptonite/myblog/Main.java
@@ -8,7 +8,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class Main extends AppCompatActivity {
 
@@ -19,13 +22,34 @@ public class Main extends AppCompatActivity {
         mBlogList =(RecyclerView)findViewById(R.id.blog_list);
         mBlogList.setHasFixedSize(true);
         mBlogList.setLayoutManager(new LinearLayoutManager(this));
- }
+    }
 
 
     @Override
     protected void onStart() {
         super.onStart();
+        FirebaseRec
+
+
     }
+
+    public static class BlogViewHoler extends  RecyclerView.ViewHolder{
+        View mView;
+
+        public BlogViewHoler(View itemView) {
+            super(itemView);
+            itemView= mView;
+        }
+        public void setTitle(String title){
+            TextView post_title =(TextView)mView.findViewById(R.id.post_title);
+            post_title.setText(title);
+        }
+        public void setDesc(String desc){
+            TextView post_desc=(TextView)mView.findViewById(R.id.post_text);
+            post_desc.setText(desc);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
